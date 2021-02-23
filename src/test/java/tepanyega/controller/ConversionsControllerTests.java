cef460ac619295ca2bfa52615e4563fe35b6076f
nicolast <nicolast@lightstoneauto.co.za>
2021-02-23 03:24:35 +0200

    added some test functions

---
+++ src/test/java/tepanyega/controller/ConversionsControllerTests.java
@@ -0,0 +1,50 @@
+package tepanyega.controller;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.tepanyega.controller.ConversionsController;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConversionsControllerTests {
+
+    @Autowired
+    private ConversionsController conversionsController;
+    private static double temperaturInKelvin, temperaturInCelsius, distanceInMiles, distanceInKilometers;
+
+    @BeforeAll
+    public static void initTestData(){
+        temperaturInKelvin = 37;
+        temperaturInCelsius = 235;
+        distanceInMiles = 987;
+        distanceInKilometers = 100;
+    }
+
+    @Test
+    public void testKelvinToCelsius(){
+        double temperature = conversionsController.kelvinToCelsius(temperaturInCelsius);
+        assertEquals(temperature, -38);
+    }
+
+    @Test
+    public void testCelsiusToKelvin(){
+        double temperature = conversionsController.celsiusToKelvin(temperaturInKelvin);
+        assertEquals(temperature, 310);
+    }
+
+    @Test
+    public void testConvertMilesToKilometers(){
+        double distance = conversionsController.milesToKilometers(distanceInMiles);
+        assertEquals(distance, 1588.41858);
+    }
+
+    @Test
+    public void testConvertKilometersToMiles(){
+        double distance = conversionsController.kilometersToMiles(distanceInKilometers);
+        assertEquals(distance, 62.137100000000004);
+    }
+}
---
+++ src/test/java/tepanyega/main/NicolastplayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package tepanyega.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicolastplayTestApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicolastplayTestApplicationTests.class, args);
+	}
+
+}
