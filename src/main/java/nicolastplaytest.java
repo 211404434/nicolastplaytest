6a0b1f399f8aa662f5a0fb7a145bfc1574f5ea82
nicolast <nicolast@lightstoneauto.co.za>
2021-02-23 03:09:37 +0200

    added the main test file

---
+++ src/main/java/nicolastplaytest.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class nicolastplaytest {
+    public static void main(String[] args) {
+        SpringApplication.run(nicolastplaytest.class, args);
+    }
+}
