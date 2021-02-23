eb6f6c88a5b7c35d2a6b80ce38ecb944e815e79c
nicolast <nicolast@lightstoneauto.co.za>
2021-02-23 02:57:13 +0200

    added two data Transfer object classes and their methods

---
+++ src/main/java/Datatransferobject/Distance.java
@@ -0,0 +1,19 @@
+package Datatransferobject;
+
+public class Distance {
+
+    private double distance;
+
+    public Distance(double distance){
+        this.distance = distance;
+    }
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
+	}
+    
+}
---
+++ src/main/java/Datatransferobject/Temperature.java
@@ -0,0 +1,19 @@
+package Datatransferobject;
+
+public class Temperature {
+
+    private double temperature;
+
+    public Temperature(double temperature){
+        this.temperature = temperature;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+    
+}
