a5dee3fac283fed136ba8de6020b6cc6bb354cc6
nicolast <nicolast@lightstoneauto.co.za>
2021-02-23 02:56:20 +0200

    added a controller

---
+++ src/main/java/com/tepanyega/controller/ConversionsController.java
@@ -0,0 +1,92 @@
+package com.tepanyega.controller;
+
+
+
+import Datatransferobject.Distance;
+import Datatransferobject.Temperature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/conversions")
+public class ConversionsController {
+
+    Logger logger = LoggerFactory.getLogger(ConversionsController.class);
+
+    @PostMapping(path = "/ktoc/{temperatureInKelvin}")
+    public Temperature kelvinToCelsius(@PathVariable(value = "temperatureInKelvin") String temperatureInKelvin) {
+        long startTime = System.currentTimeMillis();
+        double temperature = kelvinToCelsius(Double.parseDouble(temperatureInKelvin));
+        logger.info("\nExecution Time for ktoc: {} ms ", System.currentTimeMillis() - startTime);
+        return new Temperature(temperature);
+    }
+
+    @PostMapping(path = "/ctok/{temperatureInCelsius}")
+    public Temperature celsiusToKelvin(@PathVariable(value = "temperatureInCelsius") String temperatureInCelsius) {
+        long startTime = System.currentTimeMillis();
+        double temperature = celsiusToKelvin(Double.parseDouble(temperatureInCelsius));
+        logger.info("\nExecution Time for ctoc: {} ms ", System.currentTimeMillis() - startTime);
+        return new Temperature(temperature);
+    }
+
+    @PostMapping(path = "/mtok/{distanceInMiles}")
+    public Distance milesToKilometers(@PathVariable(value = "distanceInMiles") String distanceInMiles) {
+        long startTime = System.currentTimeMillis();
+        double distance = milesToKilometers(Double.parseDouble(distanceInMiles));
+        logger.info("\nExecution Time for mtok: {} ms ", System.currentTimeMillis() - startTime);
+        return new Distance(distance);
+    }
+
+    @PostMapping(path = "/ktom/{distanceInKilometers}")
+    public Distance kilometersToMiles(@PathVariable(value = "distanceInKilometers") String distanceInKilometers) {
+        long startTime = System.currentTimeMillis();
+        double distance = kilometersToMiles(Double.parseDouble(distanceInKilometers));
+        logger.info("\nExecution Time for ktom: {} ms ", System.currentTimeMillis() - startTime);
+        return new Distance(distance);
+    }
+
+    /**
+     * convert temperature from Kelvin to Celsius
+     * 
+     * @param temperaturInKelvin
+     * @return double
+     */
+    public double kelvinToCelsius(double temperaturInKelvin) {
+        return temperaturInKelvin - 273;
+    }
+
+    /**
+     * convert temperature from Celsius to Kelvin
+     * 
+     * @param temperaturInCelsius
+     * @return double
+     */
+    public double celsiusToKelvin(double temperaturInCelsius) {
+        return temperaturInCelsius + 273;
+    }
+
+    /**
+     * Convert miles into kilometers
+     * 
+     * @param distanceInMiles
+     * @return double
+     */
+    public double milesToKilometers(double distanceInMiles) {
+        return distanceInMiles * 1.60934;
+    }
+
+    /**
+     * Convert kilometers to miles
+     * 
+     * @param distanceInKilometers
+     * @return double
+     */
+    public double kilometersToMiles(double distanceInKilometers) {
+        return distanceInKilometers * 0.621371;
+    }
+
+}
